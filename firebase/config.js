bc7a18edbbd5794e44d7c213f00e63557d80d4dd
trecho26 <hectorlara260698@gmail.com>
2020-12-10 09:32:19 -0700

    Modificacion

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4KO_f_LFu5TUqkrslyq5oLKVCUbl7SuY",
+    authDomain: "product-hunt-b9ba3.firebaseapp.com",
+    projectId: "product-hunt-b9ba3",
+    storageBucket: "product-hunt-b9ba3.appspot.com",
+    messagingSenderId: "649769747402",
+    appId: "1:649769747402:web:056252fea261f0122b0533"
+};
+
+export default firebaseConfig;
